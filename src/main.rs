40120c879f063f264c010941065cb2be8dd2e9e4
Jan Hohenheim <jan@hohenheim.ch>
2025-08-07 23:12:33 +0200

    Fix lint

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,7 @@ use bitflags::bitflags;
 
 use bevy::{asset::AssetMetaCheck, audio::AudioPlugin, prelude::*, render::view::RenderLayers};
 
+#[cfg(feature = "native")]
 use bevy::core_pipeline::experimental::taa::TemporalAntiAliasPlugin;
 use oxidized_navigation::OxidizedNavigation;
 
