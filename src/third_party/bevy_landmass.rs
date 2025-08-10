feb89eedef84cd7ad55da1c3aee82e8d1202ca43
Jan Hohenheim <jan@hohenheim.ch>
2025-08-10 04:36:06 +0200

    Move to own function

--- src/third_party/bevy_landmass.rs
+++ src/third_party/bevy_landmass.rs
@@ -51,60 +51,8 @@ fn update_landmass_navmesh(
             error!("Failed to get navmesh from ID");
             continue;
         };
-        let orig = rerecast_navmesh.polygon.aabb.min;
-        let cs = rerecast_navmesh.polygon.cell_size;
-        let ch = rerecast_navmesh.polygon.cell_height;
-        let to_local = Vec3::new(cs, ch, cs);
-        let nvp = rerecast_navmesh.polygon.max_vertices_per_polygon as usize;
 
-        let landmass_navmesh = NavigationMesh3d {
-            vertices: rerecast_navmesh
-                .polygon
-                .vertices
-                .iter()
-                .map(|v| orig + v.as_vec3() * to_local)
-                .collect(),
-            polygons: (0..rerecast_navmesh.polygon.polygon_count()).fold(
-                Vec::new(),
-                |mut acc, i| {
-                    let poly = &rerecast_navmesh.polygon.polygons[i * nvp..];
-                    let verts = poly[..nvp]
-                        .iter()
-                        .filter(|i| **i != PolygonNavmesh::NO_INDEX)
-                        .map(|i| *i as usize)
-                        .collect::<Vec<_>>();
-                    acc.push(verts);
-                    acc
-                },
-            ),
-            polygon_type_indices: rerecast_navmesh
-                .polygon
-                .areas
-                .iter()
-                .map(|a| a.0 as usize)
-                .collect(),
-            height_mesh: HeightNavigationMesh3d {
-                polygons: rerecast_navmesh
-                    .detail
-                    .meshes
-                    .iter()
-                    .map(|submesh| HeightPolygon {
-                        base_vertex_index: submesh.base_vertex_index,
-                        vertex_count: submesh.vertex_count,
-                        base_triangle_index: submesh.base_triangle_index,
-                        triangle_count: submesh.triangle_count,
-                    })
-                    .collect(),
-                triangles: rerecast_navmesh.detail.triangles.clone(),
-                vertices: rerecast_navmesh
-                    .polygon
-                    .vertices
-                    .iter()
-                    .map(|v| orig + v.as_vec3() * to_local)
-                    .collect(),
-            }
-            .into(),
-        };
+        let landmass_navmesh = rerecast_to_landsmass(rerecast_navmesh);
         let landmass_navmesh = match landmass_navmesh.validate() {
             Ok(landmass_navmesh) => landmass_navmesh,
             Err(e) => {
@@ -124,4 +72,60 @@ fn update_landmass_navmesh(
             ));
     }
     Ok(())
+}
+
+fn rerecast_to_landsmass(
+    rerecast_navmesh: &bevy_rerecast::Navmesh,
+) -> bevy_landmass::NavigationMesh3d {
+    let orig = rerecast_navmesh.polygon.aabb.min;
+    let cs = rerecast_navmesh.polygon.cell_size;
+    let ch = rerecast_navmesh.polygon.cell_height;
+    let to_local = Vec3::new(cs, ch, cs);
+    let nvp = rerecast_navmesh.polygon.max_vertices_per_polygon as usize;
+
+    NavigationMesh3d {
+        vertices: rerecast_navmesh
+            .polygon
+            .vertices
+            .iter()
+            .map(|v| orig + v.as_vec3() * to_local)
+            .collect(),
+        polygons: (0..rerecast_navmesh.polygon.polygon_count()).fold(Vec::new(), |mut acc, i| {
+            let poly = &rerecast_navmesh.polygon.polygons[i * nvp..];
+            let verts = poly[..nvp]
+                .iter()
+                .filter(|i| **i != PolygonNavmesh::NO_INDEX)
+                .map(|i| *i as usize)
+                .collect::<Vec<_>>();
+            acc.push(verts);
+            acc
+        }),
+        polygon_type_indices: rerecast_navmesh
+            .polygon
+            .areas
+            .iter()
+            .map(|a| a.0 as usize)
+            .collect(),
+        height_mesh: HeightNavigationMesh3d {
+            polygons: rerecast_navmesh
+                .detail
+                .meshes
+                .iter()
+                .map(|submesh| HeightPolygon {
+                    base_vertex_index: submesh.base_vertex_index,
+                    vertex_count: submesh.vertex_count,
+                    base_triangle_index: submesh.base_triangle_index,
+                    triangle_count: submesh.triangle_count,
+                })
+                .collect(),
+            triangles: rerecast_navmesh.detail.triangles.clone(),
+            vertices: rerecast_navmesh
+                .polygon
+                .vertices
+                .iter()
+                .map(|v| orig + v.as_vec3() * to_local)
+                .collect(),
+        }
+        .into(),
+    }
 }
