ffc6ee28f3976daca175e4097881df3266f020b5
Jan Hohenheim <jan@hohenheim.ch>
2025-08-17 14:52:51 +0200

    Scope archipelagos and islands to gameplay

--- src/third_party/bevy_landmass.rs
+++ src/third_party/bevy_landmass.rs
@@ -1,15 +1,12 @@
 //! [Landmass](https://github.com/andriyDev/landmass) powers out agent navigation.
 //! The underlying navmesh is generated using [Oxidized Navigation](https://github.com/TheGrimsey/oxidized_navigation).
 
-use std::sync::Arc;
-
 use crate::{
     gameplay::{level::LevelAssets, npc::NPC_RADIUS},
     screens::Screen,
 };
 use bevy::prelude::*;
-use bevy_landmass::{ArchipelagoRef, HeightPolygon, PointSampleDistance3d, prelude::*};
-use bevy_rerecast::rerecast::PolygonNavmesh;
+use bevy_landmass::{PointSampleDistance3d, prelude::*};
 #[cfg(feature = "hot_patch")]
 use bevy_simple_subsecond_system::hot;
 use landmass_rerecast::{Island3dBundle, LandmassRerecastPlugin, NavMeshHandle3d};
@@ -19,38 +16,33 @@ pub(super) fn plugin(app: &mut App) {
         Landmass3dPlugin::default(),
         LandmassRerecastPlugin::default(),
     ));
-    app.add_systems(Startup, setup_archipelago);
-    app.add_systems(OnEnter(Screen::Gameplay), setup_island);
+    app.add_systems(OnEnter(Screen::Loading), setup_archipelago);
 }
 
 #[cfg_attr(feature = "hot_patch", hot)]
-fn setup_archipelago(mut commands: Commands) {
-    commands.spawn((
-        Name::new("Main Level Archipelago"),
-        Archipelago3d::new(AgentOptions {
-            point_sample_distance: PointSampleDistance3d {
-                horizontal_distance: 0.6,
-                distance_above: 1.0,
-                distance_below: 1.0,
-                vertical_preference_ratio: 2.0,
-            },
-            ..AgentOptions::from_agent_radius(NPC_RADIUS)
-        }),
-    ));
-}
+fn setup_archipelago(mut commands: Commands, level_assets: Res<LevelAssets>) {
+    let archipelago = commands
+        .spawn((
+            Name::new("Main Level Archipelago"),
+            StateScoped(Screen::Gameplay),
+            Archipelago3d::new(AgentOptions {
+                point_sample_distance: PointSampleDistance3d {
+                    horizontal_distance: 0.6,
+                    distance_above: 1.0,
+                    distance_below: 1.0,
+                    vertical_preference_ratio: 2.0,
+                },
+                ..AgentOptions::from_agent_radius(NPC_RADIUS)
+            }),
+        ))
+        .id();
 
-#[cfg_attr(feature = "hot_patch", hot)]
-fn setup_island(
-    mut commands: Commands,
-    archipelago: Single<Entity, With<Archipelago3d>>,
-    level_assets: Res<LevelAssets>,
-) {
     commands.spawn((
-        StateScoped(Screen::Gameplay),
         Name::new("Main Level Island"),
+        StateScoped(Screen::Gameplay),
         Island3dBundle {
             island: Island,
-            archipelago_ref: ArchipelagoRef3d::new(archipelago.into_inner()),
+            archipelago_ref: ArchipelagoRef3d::new(archipelago),
             nav_mesh: NavMeshHandle3d(level_assets.navmesh.clone()),
         },
     ));
