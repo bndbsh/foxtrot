abc1cd6f80e4e01302e0b4cb09a714a4a61f51b3
Jan Hohenheim <jan@hohenheim.ch>
2025-09-28 19:49:19 +0200

    Cargo update

--- src/gameplay/npc/ai.rs
+++ src/gameplay/npc/ai.rs
@@ -28,7 +28,7 @@ pub(super) fn plugin(app: &mut App) {
         (sync_agent_velocity, set_controller_velocity)
             .chain()
             .in_set(RunFixedMainLoopSystems::BeforeFixedMainLoop)
-            .before(LandmassSystemSet::SyncExistence)
+            .before(LandmassSystems::SyncExistence)
             .run_if(in_state(Screen::Gameplay)),
     );
     app.add_systems(
